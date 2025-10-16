a8cfca35d8fd51611b1c2a0a930931e2b1dab2f7
emiliorevv <emilio.revueltas@gmail.com>
2025-10-16 13:39:47 -0600

    first commit

---
+++ src/main/java/com/example/moneymissint/MoneyMissintApplication.java
@@ -0,0 +1,13 @@
+package com.example.moneymissint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMissintApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyMissintApplication.class, args);
+    }
+
+}
