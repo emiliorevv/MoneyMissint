2301edb1599167c2665f1935e2b08fb8fabcee99
Emilio Revueltas <emilio.revueltas@gmail.com>
2025-12-06 15:49:26 -0600

    fixed transaction request

--- src/main/java/com/example/moneymissint/DTO/TransactionRequest.java
+++ src/main/java/com/example/moneymissint/DTO/TransactionRequest.java
@@ -1,17 +1,22 @@
 package com.example.moneymissint.DTO;
 
 import com.example.moneymissint.roles.Operation;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
 public record TransactionRequest(
-        @NotBlank(message = "please add a valid amount") @Min(value = 1, message = "please enter a valid amount") @Max(value = 10000000, message = "please enter a valid amount") BigDecimal amount,
-        @NotNull(message = "operation cannot be null") Operation operation,
-        @NotBlank(message = "categoryId cannot be in blank") Long categoryId,
+        @NotNull(message = "please add a valid amount")
+        @DecimalMin(value = "0.01", message = "please enter a valid amount, amount is too small")
+        @DecimalMax(value = "10000000.0", message = "please enter a valid amount, amount is too large")
+        BigDecimal amount,
+
+
+        @NotNull(message = "operation cannot be null")
+        Operation operation,
+
+
+        @NotNull(message = "categoryId cannot be in blank") Long categoryId,
         Long destinationUserId
 ) {
 }
---
+++ src/main/java/com/example/moneymissint/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.moneymissint.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {}
